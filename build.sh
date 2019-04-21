b76a44961b5ee8fc150111b9fc90c0b145c07f47
James Larrowe <larrowe.semaj11@gmail.com>
2019-04-20 21:01:44 -0400

    Clang/LLVM support

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-gcc -m16 -c boot.S -o boot.o
+CC=${CC:=gcc}
+
+${CC} -m16 -c boot.S -o boot.o
 ld boot.o -o boot.com -Tldscripts/boot.lds
-gcc -m16 -c stage2.S -o stage2.o
+${CC} -m16 -c stage2.S -o stage2.o
 ld stage2.o -o stage2.com -Tldscripts/boot.lds
-gcc -m16 -c lib/newboot.S -o newboot.o
+${CC} -m16 -c lib/newboot.S -o newboot.o
 ar rc libnewboot.a newboot.o
 cat boot.com stage2.com > test.com
 chmod a+x test.com
